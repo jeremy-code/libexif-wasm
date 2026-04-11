88306821e8d19bf5c38b6e930a4dba529f6de06b
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-04-10 19:32:00 -0700

    fix: refactor RATIONAL and SRATIONAL handling in getDataAsTypedArray for consistent TypedArray output

--- src/libexif/utils/getDataAsTypedArray.ts
+++ src/libexif/utils/getDataAsTypedArray.ts
@@ -41,38 +41,32 @@ const getDataAsTypedArray = (
           ExifByteOrder[byteOrder],
         ),
       );
+    // For RATIONAL and SRATIONAL, instead of using the ExifRational,
+    // ExifSRational classes, since they internally just use LONGs and SLONGs
+    // for the numerator and denominator, that will be used instead, so that
+    // the output can consistently TypedArray
+    case "RATIONAL":
     case "LONG":
-      return Uint32Array.from({ length: components }, (_, index) =>
-        exif_get_long(
-          byteOffset + index * formatSize,
-          ExifByteOrder[byteOrder],
-        ),
+      return Uint32Array.from(
+        { length: format === "RATIONAL" ? components * 2 : components },
+        (_, index) =>
+          exif_get_long(
+            byteOffset +
+              index * (format === "RATIONAL" ? formatSize / 2 : formatSize),
+            ExifByteOrder[byteOrder],
+          ),
       );
     case "SLONG":
-      return Int32Array.from({ length: components }, (_, index) =>
-        exif_get_slong(
-          byteOffset + index * formatSize,
-          ExifByteOrder[byteOrder],
-        ),
-      );
-    case "RATIONAL":
-      // For RATIONAL and SRATIONAL, instead of using the ExifRational,
-      // ExifSRational classes, since they internally just use LONGs and SLONGs
-      // for the numerator and denominator, that will be used instead, so that
-      // the output can consistently TypedArray
-      return Uint32Array.from({ length: components * 2 }, (_, index) => {
-        return exif_get_long(
-          byteOffset + index * (formatSize / 2),
-          ExifByteOrder[byteOrder],
-        );
-      });
     case "SRATIONAL":
-      return Int32Array.from({ length: components * 2 }, (_, index) => {
-        return exif_get_slong(
-          byteOffset + index * (formatSize / 2),
-          ExifByteOrder[byteOrder],
-        );
-      });
+      return Int32Array.from(
+        { length: format === "SRATIONAL" ? components * 2 : components },
+        (_, index) =>
+          exif_get_slong(
+            byteOffset +
+              index * (format === "SRATIONAL" ? formatSize / 2 : formatSize),
+            ExifByteOrder[byteOrder],
+          ),
+      );
     // DOUBLE and FLOAT are unsupported, https://github.com/libexif/libexif/blob/master/libexif/exif-entry.c#L590-L591
     default:
       throw new Error("Unsupported data type");
